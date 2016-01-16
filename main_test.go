33764402c82b74a59e68ef26460ab9fa7761ba2f
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-01-16 16:08:32 +0100

    Rename the fixture data

--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestEvaluate3D(t *testing.T) {
-	interpolant, _ := New(3, TRAIN_POINTS, TRAIN_VALUES)
-	values := interpolant.Evaluate(TEST_POINTS)
-	assert.EqualWithin(values, TEST_VALUES, 1e-15, t)
+	interpolant, _ := New(3, trainPoints, trainValues)
+	values := interpolant.Evaluate(testPoints)
+	assert.EqualWithin(values, testValues, 1e-15, t)
 }
