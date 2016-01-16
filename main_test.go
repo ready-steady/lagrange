06a2ccfc6b39a87fbb9e7ef4ea06ce0a48a22e2f
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-01-16 16:06:30 +0100

    Add a failing test

--- main_test.go
+++ main_test.go
@@ -2,7 +2,12 @@ package lagrange
 
 import (
 	"testing"
+
+	"github.com/ready-steady/assert"
 )
 
-func TestEvaluate(_ *testing.T) {
+func TestEvaluate3D(t *testing.T) {
+	interpolant, _ := New(3, TRAIN_POINTS, TRAIN_VALUES)
+	values := interpolant.Evaluate(TEST_POINTS)
+	assert.EqualWithin(values, TEST_VALUES, 1e-15, t)
 }
